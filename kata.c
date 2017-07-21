d90245dda51cf6b6b820e92e66997e4dde324695
karthik59 <30342623+karthik59@users.noreply.github.com>
2017-07-21 17:01:12 +0530

    Create kata.c

---
+++ kata.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int number;
+printf("enter the number\t");
+scanf("%d",&number);
+}
+{
+if(number<0)
+printf("the entered number is negative");
+else
+printf("the entered number is positive");
+}
+else
+{
+printf("the entered number is zero");
+}
